5a95631970a756428b73827918c2d9c891f020a3
Srinath Setty <srinath@microsoft.com>
2021-03-04 17:12:43 -0800

    add a failing test case

--- src/tests.rs
+++ src/tests.rs
@@ -16,6 +16,8 @@ use ft::*;
 use itertools::iterate;
 use merlin::Transcript;
 use rand::Rng;
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 use sha3::Sha3_256;
 use std::iter::repeat_with;
 
@@ -220,6 +222,103 @@ fn end_to_end() {
     .unwrap();
 
     assert_eq!(res, eval);
+}
+
+#[test]
+fn end_to_end_two_proofs() {
+    use super::{commit, prove, verify};
+
+    // commit to a random polynomial at a random rate
+    let (coeffs, rho) = random_coeffs_rho();
+    let n_col_opens = 128usize;
+    let comm = commit::<Sha3_256, _>(&coeffs, rho, n_col_opens).unwrap();
+    // this is the polynomial commitment
+    let root = comm.get_root().unwrap();
+
+    // evaluate the random polynomial we just generated at a random point x
+    let x = Ft::random(&mut rand::thread_rng());
+    let eval = comm
+        .coeffs
+        .iter()
+        .zip(iterate(Ft::one(), |&v| v * x).take(coeffs.len()))
+        .fold(Ft::zero(), |acc, (c, r)| acc + *c * r);
+
+    // compute the outer and inner tensors for powers of x
+    // NOTE: we treat coeffs as a univariate polynomial, but it doesn't
+    // really matter --- the only difference from a multilinear is the
+    // way we compute outer_tensor and inner_tensor from the eval point
+    let inner_tensor: Vec<Ft> = iterate(Ft::one(), |&v| v * x)
+        .take(comm.n_per_row)
+        .collect();
+    let outer_tensor: Vec<Ft> = {
+        let xr = x * inner_tensor.last().unwrap();
+        iterate(Ft::one(), |&v| v * xr).take(comm.n_rows).collect()
+    };
+
+    // compute an evaluation proof
+    let mut tr1 = Transcript::new(b"test transcript");
+    tr1.append_message(b"polycommit", root.as_ref());
+    tr1.append_message(b"rate", &rho.to_be_bytes()[..]);
+    tr1.append_message(b"ncols", &(n_col_opens as u64).to_be_bytes()[..]);
+    let pf = prove::<Sha3_256, _>(&comm, &outer_tensor[..], &mut tr1).unwrap();
+
+    let challenge_after_first_proof_prover = {
+        let mut key: <ChaCha20Rng as SeedableRng>::Seed = Default::default();
+        tr1.challenge_bytes(b"ligero-pc//challenge", &mut key);
+        let mut deg_test_rng = ChaCha20Rng::from_seed(key);
+        Ft::random(&mut deg_test_rng)
+    };
+
+    // produce a second proof with the same transcript
+    tr1.append_message(b"polycommit", root.as_ref());
+    tr1.append_message(b"rate", &rho.to_be_bytes()[..]);
+    tr1.append_message(b"ncols", &(n_col_opens as u64).to_be_bytes()[..]);
+    let pf2 = prove::<Sha3_256, _>(&comm, &outer_tensor[..], &mut tr1).unwrap();
+
+    // verify it and finish evaluation
+    let mut tr2 = Transcript::new(b"test transcript");
+    tr2.append_message(b"polycommit", root.as_ref());
+    tr2.append_message(b"rate", &rho.to_be_bytes()[..]);
+    tr2.append_message(b"ncols", &(n_col_opens as u64).to_be_bytes()[..]);
+    let res = verify(
+        &root,
+        &outer_tensor[..],
+        &inner_tensor[..],
+        &pf,
+        rho,
+        n_col_opens,
+        &mut tr2,
+    )
+    .unwrap();
+
+    assert_eq!(res, eval);
+    let challenge_after_first_proof_verifier = {
+        let mut key: <ChaCha20Rng as SeedableRng>::Seed = Default::default();
+        tr2.challenge_bytes(b"ligero-pc//challenge", &mut key);
+        let mut deg_test_rng = ChaCha20Rng::from_seed(key);
+        Ft::random(&mut deg_test_rng)
+    };
+    assert_eq!(
+        challenge_after_first_proof_prover,
+        challenge_after_first_proof_verifier
+    );
+
+    // second proof verification with the same transcript
+    tr2.append_message(b"polycommit", root.as_ref());
+    tr2.append_message(b"rate", &rho.to_be_bytes()[..]);
+    tr2.append_message(b"ncols", &(n_col_opens as u64).to_be_bytes()[..]);
+    let res2 = verify(
+        &root,
+        &outer_tensor[..],
+        &inner_tensor[..],
+        &pf2,
+        rho,
+        n_col_opens,
+        &mut tr2,
+    )
+    .unwrap();
+
+    assert_eq!(res2, eval);
 }
 
 fn random_coeffs_rho() -> (Vec<Ft>, f64) {
