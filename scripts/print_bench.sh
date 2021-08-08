6e0d78ed8bf1f30104181166c8d051cebf4d0264
kwantam <kwantam@gmail.com>
2021-08-08 17:29:45 -0400

    print_bench script

---
+++ scripts/print_bench.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -u
+
+CTN="$1".txt
+PVS="$1"_pvs.txt
+
+grep '^[0-9][0-9]:' $CTN | awk '{print($2/1e9);}' > ~/tmp/foo_ctime
+grep '^[0-9][0-9]:' $PVS | awk '{print($2/1e9);}' > ~/tmp/foo_ptime
+grep '^[0-9][0-9]:' $PVS | awk '{print($3/1e9);}' > ~/tmp/foo_vtime
+grep '^[0-9][0-9]:' $PVS | awk '{print($4/1024);}' > ~/tmp/foo_comm
